aefd6e0fe153539b5014509a2553f4b125489aae
Daniel C. Wilson <dwilson1@gmail.com>
2013-09-22 16:39:54 -0500

    JS structure

--- analytics.js
+++ analytics.js
@@ -8,15 +8,8 @@ UniversalAnalyticsPlugin.prototype.trackView = function(screen) {
 	cordova.exec(function() {}, function() {}, 'UniversalAnalytics', 'trackView', [screen]);
 };
 
-UniversalAnalyticsPlugin.prototype.trackEvent = function(category,action,label) {
-	var options = {category:category,
-		action:action,
-		label:label,
-		value:isNaN(parseInt(value)) ? -1 : value};
-	cordova.exec(function() {}, function() {}, 'UniversalAnalytics', 'trackEvent',[category, action, label]);
+UniversalAnalyticsPlugin.prototype.trackEvent = function(category, action, label) {
+	cordova.exec(function() {}, function() {}, 'UniversalAnalytics', 'trackEvent', [category, action, label]);
 };
 
-cordova.addConstructor(function() {
-  if(!window.plugins) window.plugins = {};
-  window.plugins.universalAnalyticsPlugin = new UniversalAnalyticsPlugin();
-});
+module.exports = new UniversalAnalyticsPlugin();
