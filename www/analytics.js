48d6b96459bdfd6c8724bade5ab616c638bf2daf
Daniel C. Wilson <dwilson1@gmail.com>
2014-04-22 09:49:50 -0500

    cleanup/linting

--- www/analytics.js
+++ www/analytics.js
@@ -1,26 +1,24 @@
-function UniversalAnalyticsPlugin() {};
+function UniversalAnalyticsPlugin() {}
 
 UniversalAnalyticsPlugin.prototype.startTrackerWithId = function(id, success, error) {
-	cordova.exec(success, error, 'UniversalAnalytics', 'startTrackerWithId', [id]);
+  cordova.exec(success, error, 'UniversalAnalytics', 'startTrackerWithId', [id]);
 };
 
 UniversalAnalyticsPlugin.prototype.trackView = function(screen, success, error) {
-	cordova.exec(success, error, 'UniversalAnalytics', 'trackView', [screen]);
+  cordova.exec(success, error, 'UniversalAnalytics', 'trackView', [screen]);
 };
 
 UniversalAnalyticsPlugin.prototype.addCustomDimension = function(key, value, success, error) {
-	cordova.exec(success, error, 'UniversalAnalytics', 'addCustomDimension', [key, value]);
+  cordova.exec(success, error, 'UniversalAnalytics', 'addCustomDimension', [key, value]);
 };
 
 UniversalAnalyticsPlugin.prototype.trackEvent = function(category, action, label, value, success, error) {
-
-	if ( typeof label === 'undefined' || label === null )
-		label = '';
-
-	if ( typeof value === 'undefined' || value === null )
+  if (typeof label === 'undefined' || label === null) {
+    label = '';
+  }
+	if (typeof value === 'undefined' || value === null) {
 		value = 0;
-
-	var args = [ category , action , label , value ];
+  }
 
 	cordova.exec(success, error, 'UniversalAnalytics', 'trackEvent', [category, action, label, value]);
 };
