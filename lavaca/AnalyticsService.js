f960b5d5a766ef1e6f5585c702a206cffbfaa452
Stephen Bartholomew <stephenbartholomew@gmail.com>
2014-05-13 14:35:52 +0100

    Added not implemented message for lavaca

--- lavaca/AnalyticsService.js
+++ lavaca/AnalyticsService.js
@@ -48,6 +48,9 @@ define(function(require) {
     setUserId: function() {
       throw 'setUserId is not implemented for Lavaca';
     },
+    debugMode: function() {
+      throw 'debugMode is not implemented for Lavaca';
+    },
     trackEvent: function(category, action, label, value) {
       action = action || '';
       label = label || '';
