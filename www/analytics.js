7372873a312b4704485c8d3d6fbb509064cc9695
Jose Medaglia <jmedaglia@gmail.com>
2014-11-05 08:10:55 -0600

    Adding TrackException support (Android only for now) Standarized indentation (spaces)

--- www/analytics.js
+++ www/analytics.js
@@ -25,11 +25,18 @@ UniversalAnalyticsPlugin.prototype.trackEvent = function(category, action, label
   if (typeof label === 'undefined' || label === null) {
     label = '';
   }
-	if (typeof value === 'undefined' || value === null) {
-		value = 0;
+  if (typeof value === 'undefined' || value === null) {
+    value = 0;
   }
 
-	cordova.exec(success, error, 'UniversalAnalytics', 'trackEvent', [category, action, label, value]);
+  cordova.exec(success, error, 'UniversalAnalytics', 'trackEvent', [category, action, label, value]);
+};
+
+/**
+ * https://developers.google.com/analytics/devguides/collection/android/v3/exceptions
+ */
+UniversalAnalyticsPlugin.prototype.trackException = function(description, fatal, success, error) {
+  cordova.exec(success, error, 'UniversalAnalytics', 'trackException', [description, fatal]);
 };
 
 /* Google Analytics e-Commerce Tracking */
